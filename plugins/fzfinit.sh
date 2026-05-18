ded2c3e4d241405eec4fc65910a6c614dbfa6178
awesome <92878934+awesomemad@users.noreply.github.com>
2026-05-18 11:12:34 +0300

    Deleted plugins directory for
    Deleted the plugins directory to separate the registry from the code.

--- plugins/fzfinit.sh
+++
@@ -1,2 +0,0 @@
-source /ucrt64/share/fzf/key-bindings.bash
-source /ucrt64/share/fzf/completion.bash
--- plugins/sysdo.sh
+++
@@ -1,6 +0,0 @@
-sudo() {
-  runas /user:Administrator "$@"
-}
-sysdo() {
-  /c/users/keleb/psexec64 -i -d -s $@
-}
